02873c6a14c1ea2b7c09f58bdf7c6fed72dd0522
matthew-pisano <matthewpisano14@gmail.com>
2026-03-04 00:31:21 -0500

    Use Opcode Enum in Test

--- test/test_bytecode.cpp
+++ test/test_bytecode.cpp
@@ -8,10 +8,25 @@
 #include "bytecode.h"
 #include "pythoncode.h"
 
+/**
+ * Extracts a vector of opcodes from a given vector of instruction
+ * @param instructions The vector of instructions
+ * @return A vector of opcodes from the instructions
+ */
+std::vector<PythonOpcode> extractInstructionOpcodes(const std::vector<Instruction>& instructions) {
+    std::vector<PythonOpcode> opcodes;
+    for (Instruction inst : instructions)
+        opcodes.push_back(inst.opcode);
+    return opcodes;
+}
+
 TEST_CASE("Empty Bytecode") {
-    const std::string source;
+    const std::string source; // Blank source
+    const std::vector expectedOpcodes = {PythonOpcode::RESUME, PythonOpcode::LOAD_CONST, PythonOpcode::RETURN_VALUE};
     const CompiledModule compiledModule = compilePythonSource(source, "<embedded>", "<embedded>");
     const ByteCodeModule bytecodeModule = generatePythonBytecode(compiledModule);
+    const std::vector<PythonOpcode> opcodes = extractInstructionOpcodes(bytecodeModule.instructions);
 
     REQUIRE(bytecodeModule.instructions.size() == 3);
+    REQUIRE(opcodes == expectedOpcodes);
 }

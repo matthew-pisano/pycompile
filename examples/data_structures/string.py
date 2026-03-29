7fe046c65d678fe88fa63795aeed998eeec29277
matthew-pisano <matthewpisano14@gmail.com>
2026-03-29 13:30:16 -0400

    Refine Example Scripts

---
+++ examples/data_structures/string.py
@@ -0,0 +1,2 @@
+print("String Concatenation:", "Test Num: " + str(2))
+print("F-String:", f"Formatted Num: <{3}>")
---
+++ examples/logic/arithmetic.py
@@ -0,0 +1,7 @@
+print("Addition:", 1 + 2)
+print("Division:", 4.2 / 2)
+print("Exponentiation:", 2 ** 4)
+print("Modulo:", 4 % 3)
+
+a = 1  # Used to prevent interpreter inlining
+print("Integer Negation:", -a)

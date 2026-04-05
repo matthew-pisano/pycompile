ea4c46b21f4ed27a7453c62696ec419b3e0ccc18
matthew-pisano <matthewpisano14@gmail.com>
2026-04-05 01:00:29 -0400

    Add Tuple Example File

---
+++ examples/data_structures/tuple.py
@@ -0,0 +1,17 @@
+print("Empty Tuple:", tuple())
+
+print("Short Tuple:", (1, 2, 3))
+
+print("Long Tuple:", (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+
+print("Tuple Addition:", (1, 2) + (3,))
+
+print("Tuple Length:", len((1, 2, 3, 4)))
+
+print("Tuple Index:", (1, 2)[0])
+
+print("Tuple Membership:", 0 in (1, 2))
+
+print("Tuple of Strings:", tuple("Hello"))
+
+print("Bool Tuples:", bool(tuple()), bool((1,)))

d4949d731e1c712fd74f28938f11c09b968ba704
matthew-pisano <matthewpisano14@gmail.com>
2026-04-07 23:52:30 -0400

    Expand Iter Example

--- examples/data_structures/iter.py
+++ examples/data_structures/iter.py
@@ -1,7 +1,34 @@
-a = [1, 2, 3, 4]
+a = "123"
+print("Iterate over str:", a)
 it = iter(a)
 print(next(it))
 print(next(it))
+print(next(it))
+
+a = [1, 2, 3]
+print("Iterate over list:", a)
+it = iter(a)
+print(next(it))
+print(next(it))
+print(next(it))
+
+a = {1, 2, 3}
+print("Iterate over set:", a)
+it = iter(a)
+print(next(it))
+print(next(it))
+print(next(it))
+
+a = {1: 'one', 2: 'two', 3: 'three'}
+print("Iterate over dict:", a)
+it = iter(a)
+print(next(it))
+print(next(it))
+print(next(it))
+
+a = (1, 2, 3)
+print("Iterate over tuple:", a)
+it = iter(a)
 print(next(it))
 print(next(it))
 print(next(it))

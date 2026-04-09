7cf43bd335fbec33daf58a15ac6da4f76ae94937
matthew-pisano <matthewpisano14@gmail.com>
2026-04-08 20:14:40 -0400

    Add Iteration Tests for all Data Structures

--- test/execution/test_iter_execution.cpp
+++ test/execution/test_iter_execution.cpp
@@ -36,7 +36,31 @@ TEST_CASE_METHOD(JITFixture, "Test JIT Empty Iterator") {
     REQUIRE_THROWS_WITH(runCapture("a = iter([])\nnext(a)"), "StopIteration()");
 }
 
-TEST_CASE_METHOD(JITFixture, "Test JIT Iterator") {
+TEST_CASE_METHOD(JITFixture, "Test JIT Str Iteration") {
+    const std::string output = runCapture("a = iter('123')\nprint(next(a))\nprint(next(a))\nprint(next(a))");
+    REQUIRE(output == "1\n2\n3\n");
+}
+
+TEST_CASE_METHOD(JITFixture, "Test JIT List Iteration") {
     const std::string output = runCapture("a = iter([1, 2, 3])\nprint(next(a))\nprint(next(a))\nprint(next(a))");
+    REQUIRE(output == "1\n2\n3\n");
+}
+
+TEST_CASE_METHOD(JITFixture, "Test JIT Set Iteration") {
+    const std::string output = runCapture("a = iter({1, 2, 3})\nprint(next(a))\nprint(next(a))\nprint(next(a))");
+    REQUIRE(output.length() == 6);
+    REQUIRE(output.contains("1\n"));
+    REQUIRE(output.contains("2\n"));
+    REQUIRE(output.contains("3\n"));
+}
+
+TEST_CASE_METHOD(JITFixture, "Test JIT Dict Iteration") {
+    const std::string output =
+            runCapture("a = iter({1: 'one', 2: 'two', 3: 'three'})\nprint(next(a))\nprint(next(a))\nprint(next(a))");
+    REQUIRE(output == "1\n2\n3\n");
+}
+
+TEST_CASE_METHOD(JITFixture, "Test JIT Tuple Iteration") {
+    const std::string output = runCapture("a = iter((1, 2, 3))\nprint(next(a))\nprint(next(a))\nprint(next(a))");
     REQUIRE(output == "1\n2\n3\n");
 }

583e8a0b6e439a270b70deb4e0c47c2edb0d52cb
matthew-pisano <matthewpisano14@gmail.com>
2026-04-11 16:12:28 -0400

    Implement While loop test

--- test/execution/test_code_execution.cpp
+++ test/execution/test_code_execution.cpp
@@ -21,3 +21,8 @@ TEST_CASE_METHOD(JITFixture, "Test JIT Function With Arg") {
     const std::string output = runCapture("def foo(arg):\n  print(arg)\nfoo('bar')");
     REQUIRE(output == "bar\n");
 }
+
+TEST_CASE_METHOD(JITFixture, "Test JIT While") {
+    const std::string output = runCapture("i = 0\nwhile i < 3:\n  print(i)\n  i = i + 1");
+    REQUIRE(output == "0\n1\n2\n");
+}

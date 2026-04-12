e784831939cc7407dc0b58df39730e1d71faa628
matthew-pisano <matthewpisano14@gmail.com>
2026-04-12 14:47:03 -0400

    Add Nested Loop Examples

--- examples/control_flow/for.py
+++ examples/control_flow/for.py
@@ -1,2 +1,8 @@
+print("Single For loop:")
 for i in range(10):
     print("Index:", i)
+
+print("Nested For loop:")
+for i in range(3):
+    for j in range(3):
+        print("Index:", i, j)
--- examples/control_flow/while.py
+++ examples/control_flow/while.py
@@ -1,4 +1,14 @@
+print("Single While loop:")
 i = 0
 while i < 10:
     print("Index:", i)
+    i += 1
+
+print("Nested While loop:")
+i = 0
+j = 0
+while i < 3:
+    while j < 3:
+        print("Index:", i, j)
+        j += 1
     i += 1

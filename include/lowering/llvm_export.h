7ac9fc556ccacafb6d35eacbfc9da81e6fd45a58
matthew-pisano <matthewpisano14@gmail.com>
2026-04-17 18:44:22 -0400

    Add Debug and Optimization Options

--- include/lowering/llvm_export.h
+++ include/lowering/llvm_export.h
@@ -23,6 +23,9 @@ struct LLVMExportOptions {
 
     /// Optimization level: 0 = none, 1 = less, 2 = default, 3 = aggressive.
     unsigned int optLevel = 0;
+
+    /// Whether ot generate a debug build.
+    bool debugInfo = false;
 };
 
 

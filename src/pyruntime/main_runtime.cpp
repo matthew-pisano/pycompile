90a38fb6da23807020d8afa867d3cc477c13a071
matthew-pisano <matthewpisano14@gmail.com>
2026-04-18 12:56:59 -0400

    Add File Information to Runtime Errors

--- src/pyruntime/main_runtime.cpp
+++ src/pyruntime/main_runtime.cpp
@@ -4,6 +4,9 @@
 
 #include <iostream>
 
+#include "pyruntime/builtin_runtime.h"
+#include "pyruntime/objects/py_str.h"
+
 extern "C" void __pymodule(); // Provided by the translated MLIR module
 
 int main() {
@@ -12,6 +15,9 @@ int main() {
         return 0;
     } catch (std::runtime_error& e) {
         std::cerr << "Traceback (most recent call last):" << std::endl;
+        const PyStr* file = dynamic_cast<PyStr*>(pyir_builtinVarFile());
+        const PyStr* name = dynamic_cast<PyStr*>(pyir_builtinVarName());
+        std::cerr << std::format("    File \"{}\", in {}", file->data(), name->data()) << std::endl;
         std::cerr << e.what() << std::endl;
         return 1;
     }

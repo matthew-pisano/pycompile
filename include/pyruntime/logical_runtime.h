446ea172d9acdad5e704bb9148c4468f97391d90
matthew-pisano <matthewpisano14@gmail.com>
2026-04-18 17:44:18 -0400

    Manage Memory in Logical Operations

--- include/pyruntime/logical_runtime.h
+++ include/pyruntime/logical_runtime.h
@@ -14,59 +14,59 @@ struct PyFloat;
 struct PyObj;
 struct PyInt;
 
-int8_t pyir_isTruthy(const PyObj* val);
+int8_t pyir_isTruthy(PyObj* val);
 
 // arithmetic
-PyObj* pyir_add(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_add(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_sub(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_sub(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_mul(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_mul(PyObj* lhs, PyObj* rhs);
 
-PyFloat* pyir_div(const PyObj* lhs, const PyObj* rhs);
+PyFloat* pyir_div(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_floorDiv(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_floorDiv(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_exp(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_exp(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_mod(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_mod(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_xor(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_xor(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_pipe(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_pipe(PyObj* lhs, PyObj* rhs);
 
-PyObj* pyir_ampersand(const PyObj* lhs, const PyObj* rhs);
+PyObj* pyir_ampersand(PyObj* lhs, PyObj* rhs);
 
 // index
-PyObj* pyir_idx(const PyObj* obj, const PyObj* idx);
+PyObj* pyir_idx(PyObj* obj, PyObj* idx);
 
 // membership
-PyBool* pyir_in(const PyObj* container, const PyObj* element);
+PyBool* pyir_in(PyObj* container, PyObj* element);
 
 // comparison
-PyBool* pyir_eq(const PyObj* lhs, const PyObj* rhs);
+PyBool* pyir_eq(PyObj* lhs, PyObj* rhs);
 
-PyBool* pyir_ne(const PyObj* lhs, const PyObj* rhs);
+PyBool* pyir_ne(PyObj* lhs, PyObj* rhs);
 
-PyBool* pyir_lt(const PyObj* lhs, const PyObj* rhs);
+PyBool* pyir_lt(PyObj* lhs, PyObj* rhs);
 
-PyBool* pyir_le(const PyObj* lhs, const PyObj* rhs);
+PyBool* pyir_le(PyObj* lhs, PyObj* rhs);
 
-PyBool* pyir_gt(const PyObj* lhs, const PyObj* rhs);
+PyBool* pyir_gt(PyObj* lhs, PyObj* rhs);
 
-PyBool* pyir_ge(const PyObj* lhs, const PyObj* rhs);
+PyBool* pyir_ge(PyObj* lhs, PyObj* rhs);
 
 // unary operations
-PyObj* pyir_unaryNegative(const PyObj* val);
+PyObj* pyir_unaryNegative(PyObj* val);
 
-PyObj* pyir_unaryNot(const PyObj* val);
+PyObj* pyir_unaryNot(PyObj* val);
 
-PyInt* pyir_unaryInvert(const PyObj* val);
+PyInt* pyir_unaryInvert(PyObj* val);
 
 // truthiness, used by conditional jumps
-PyBool* pyir_toBool(const PyObj* val);
+PyBool* pyir_toBool(PyObj* val);
 
-PyStr* pyir_formatSimple(const PyObj* val);
+PyStr* pyir_formatSimple(PyObj* val);
 }
 
 #endif // PYCOMPILE_LOGICAL_RUNTIME_H

e525e30029e2a216cce72d87c9ed94d44fb75a85
matthew-pisano <matthewpisano14@gmail.com>
2026-04-18 20:18:01 -0400

    Delete Immortal Objects in Main

--- src/pyruntime/main_runtime.cpp
+++ src/pyruntime/main_runtime.cpp
@@ -6,6 +6,8 @@
 #include <ranges>
 
 #include "pyruntime/builtin_runtime.h"
+#include "pyruntime/objects/py_bool.h"
+#include "pyruntime/objects/py_none.h"
 #include "pyruntime/objects/py_str.h"
 
 extern "C" void __pymodule(); // Provided by the translated MLIR module
@@ -13,6 +15,11 @@ extern "C" void __pymodule(); // Provided by the translated MLIR module
 int main() {
     try {
         __pymodule();
+        
+        // Delete immortal objects
+        delete PyNone::None;
+        delete PyBool::True;
+        delete PyBool::False;
         return 0;
     } catch (const std::exception& e) {
         std::cerr << "Traceback (most recent call last):" << std::endl;
